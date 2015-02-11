f775646e35326f18eec01924be937e9b7a63563d
Craig Wann <craigwann1@gmail.com>
2015-02-10 23:15:37 -0600

    Adding simple drawer and bootstrap multi select.

--- resources/js/main.js
+++ resources/js/main.js
@@ -8,20 +8,74 @@ $(window).load(function(){
 
 function process_content(item) {
 
-    /*============================================
-    Drawer
-    ==============================================*/
-
-    jQuery(item).find('.drawer').each(function(index, el) {
-        var opts = jQuery(el).attr('drawer-options');
-        if (opts) {
-            var options = opts.parseJSON();
-        } else {
-            var options = {};
-        }
-        var drawer = new Drawer(el, options);
+    jQuery(item).find('.bootbox-html').each(function(index, el) {
+        var target = el.getAttribute('bootbox-target');
+        var message = $(target).html();
+        jQuery(el).on( "click", function() {
+            bootbox.dialog({
+                title: el.getAttribute('bootbox-title'),
+                message: message,
+            });
+        });
+        $(document).on("shown.bs.modal", function (event) {
+            process_content(event.target);
+        });
     });
 
+    jQuery(item).find('.bootbox-confirm').each(function(index, el) {
+        var message = jQuery(el).attr('confirm-message');
+        var href = jQuery(el).attr('href');
+
+        if (!message) {
+            message = "Are you sure?";
+        }
+
+        jQuery(el).on( "click", function(event) {
+            event.preventDefault();
+            bootbox.confirm(message, function (result) {
+                if (result) {
+                    if (href) {
+                        window.location.href = href;
+                    }
+                }
+            });
+        });
+
+    });
+
+    jQuery(item).find('.drawer').each(function(index, el) {
+        new SimpleDrawer(el);
+    });
+
+    jQuery(item).find('select[multiple]').each(function(index, el) {
+        var placeholder = (el.getAttribute('placeholder') ? el.getAttribute('placeholder') : "No option selected...");
+        var enableFiltering = (el.getAttribute('multiselect-filtering') ? el.getAttribute('multiselect-filtering') : false);
+
+        jQuery(el).multiselect({
+            enableFiltering: enableFiltering,
+            filterPlaceholder: 'Search...',
+            buttonText: function(options, select) {
+                    if (options.length === 0) {
+                        return placeholder;
+                    }
+                    else if (options.length > 6) {
+                        return "Woah, that's a lot of selections." ;
+                    }
+                    else {
+                        var labels = [];
+                        options.each(function() {
+                            if ($(this).attr('label') !== undefined) {
+                                labels.push($(this).attr('label'));
+                            }
+                            else {
+                                labels.push($(this).html());
+                            }
+                        });
+                        return labels.join(', ') + ' ';
+                    }
+                }
+        });
+    });
 }
 
 $(document).ready(function() {
--- resources/views/milestone/form.blade.php
+++ resources/views/milestone/form.blade.php
@@ -15,9 +15,8 @@
 <h2>What does it do?</h2>
 
 <div class="drawer">
-
-    {!! Form::openGroup('rewards_ability', 'It awards an ability.') !!}
-    {!! Form::checkbox('rewards_ability', null, null, array('class' => 'drawer-toggle')) !!}
+    {!! Form::openGroup('rewards_ability') !!}
+    {!! Form::inlineCheckbox('rewards_ability', null, 'It awards an ability.', false, ['class' => 'drawer-toggle']) !!}
     {!! Form::closeGroup() !!}
 
     <div class="drawer-target">
@@ -26,23 +25,24 @@
         {!! Form::closeGroup() !!}
 
         {!! Form::openGroup('targets[]') !!}
-        {!! Form::select('targets[]', $targetOptions, array('multiple' => true)) !!}
+        {!! Form::select('targets[]', $targetOptions, null, array('multiple' => true, 'placeholder' => 'Select Targets')) !!}
         {!! Form::closeGroup() !!}
 
         {!! Form::openGroup('ranges[]') !!}
-        {!! Form::select('ranges[]', $rangeOptions, array('multiple' => true)) !!}
+        {!! Form::select('ranges[]', $rangeOptions, null, array('multiple' => true, 'placeholder' => 'Select Ranges')) !!}
         {!! Form::closeGroup() !!}
 
         {!! Form::openGroup('attunements[]') !!}
-        {!! Form::select('attunements[]', $attunementOptions, array('multiple' => true)) !!}
+        {!! Form::select('attunements[]', $attunementOptions, null, array('multiple' => true, 'placeholder' => 'Select Attunements')) !!}
         {!! Form::closeGroup() !!}
     </div>
 </div>
 
 <div class="drawer">
-    {!! Form::openGroup('rewards_attribute', 'It awards an attribute modifier.') !!}
-    {!! Form::checkbox('rewards_attribute', null, null, array('class' => 'drawer-toggle')) !!}
+    {!! Form::openGroup('rewards_attribute') !!}
+        {!! Form::inlineCheckbox('rewards_attribute', null, 'It awards an attribute modifier.', false, ['class' => 'drawer-toggle']) !!}
     {!! Form::closeGroup() !!}
+
 
     <div class="drawer-target">
         <div class="clone-container">
